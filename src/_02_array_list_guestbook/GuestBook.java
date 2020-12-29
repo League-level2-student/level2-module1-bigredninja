d0a6231b33cc5ee4eacb1c9898a893d6f11bd258
C2SDK <C2SDK@C47427>
2020-12-29 14:57:52 -0800

    asdfghjkl

--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,10 +1,21 @@
 package _02_array_list_guestbook;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook {
+
+public class GuestBook implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton b1 = new JButton("Add Name");
+	JButton b2 = new JButton("View Name");
+	ArrayList<String> names = new ArrayList<String>();
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -14,14 +25,29 @@ public class GuestBook {
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-			JButton b1 = new JButton("Add Name");
-		JButton b2 = new JButton("View Name");
-		b1.addMouseListener(null);
-		b2.addMouseListener(null);		
-	
-	
-	}
+		new GuestBook().showButton();
 		
+	}
+	public void showButton(){
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        frame.add(panel);
+        panel.add(b1);
+        panel.add(b2);
+        frame.pack();
+        frame.setVisible(true);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getSource() == b1) {
+			String addName = JOptionPane.showInputDialog("Give me a name you want to add to the guest book");
+			names.add(addName);
+		}
+		if (arg0.getSource() == b2) {
+			 JOptionPane.showMessageDialog(null, names);
+			 
+		}
+	}
 }
